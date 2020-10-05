493df279f253840fd3c348e262c21b042deff1c3
Kadamir24 <amir.kaniyev@gmail.com>
2020-10-05 13:49:30 +0600

    feat: dinish decoding

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let counter = 0;
+    expr = expr.replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');
+    newStr = '';  
+    for (let i = 0; i < expr.length - 1; i++) {
+
+        if (expr[i] == '1' && expr[i+1] == '0') {
+            newStr += '.';
+            i++
+        } else if (expr[i] == '1' && expr[i+1] == '1') {
+            newStr += '-';
+            i++;
+        } else if (expr[i] == ' ') {
+            newStr += '   ';
+            counter = 0;
+            continue;
+        } else {
+            i++
+        }
+
+
+        counter++
+        if (counter == 5) {
+            newStr += ' ';
+            counter = 0;
+        }
+    }
+
+    let decoding = [];
+    newStr.split('   ').map( (word) => {
+        word.split(' ').map( (letter) => {
+            decoding.push(MORSE_TABLE[letter]);
+        });
+        decoding.push(' ');
+    });
     // write your solution here
+    decodingStr = decoding.join("");
+    decodingStr = decodingStr.replace(/\s+/g, ' ').trim();
+    return decodingStr;
+
 }
 
 module.exports = {
